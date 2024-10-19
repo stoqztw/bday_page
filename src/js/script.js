e5e60a10b7cf7407c739f747e658d28716709329
atsq <ap3994741@gmail.com>
2024-10-19 23:00:18 +0300

    complete project

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+function showMessage() {
+    // Скрыть подарок
+    document.getElementById('gift').style.display = 'none';
+    
+    // Показать поздравление
+    document.getElementById('congrats').style.display = 'block';
+  }
+  
+const hero_btn = document.querySelector('.hero_btn');
+
+hero_btn.addEventListener('click', () => {
+  const duration = 15 * 1000,
+  animationEnd = Date.now() + duration,
+  defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const interval = setInterval(function() {
+  const timeLeft = animationEnd - Date.now();
+
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+
+  const particleCount = 50 * (timeLeft / duration);
+
+  // since particles fall down, start a bit higher than random
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    })
+  );
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    })
+  );
+}, 250);
+})
